8ee0ca3c78496b882855639b37b82f2cc74241b0
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-10-10 22:24:51 +0100

    reset CRA and added class component

---
+++ src/components/ClassComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <h2>This is a class based compoent</h2>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
