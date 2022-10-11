b1b95917ecbd54e54bd137ba4d5ae57c69401042
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-10-11 12:32:30 +0100

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "../src/styles/App.css";
 import React from "react";
-import ReactDOM from "react-dom/client";
 import Render from "../src/components/Render";
 import ClassComponent from "./components/ClassComponent";
 
--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -1,14 +1,39 @@
 import React from "react";
-
+// const ChildComponent = () => {
+//   return <div>I am a Child</div>;
+// };
+const TypesOfCheese = () => {
+  return (
+    <>
+      <h2>Types of Cheeses</h2>
+      <ul>
+        <li>Cheddar</li>
+        <li>Swiss</li>
+        <li>Provolone</li>
+        <li>Manchego</li>
+      </ul>
+      {/* Use React to Render Nested Component */}
+    </>
+  );
+};
+const Cheese = () => {
+  return (
+    <>
+      <TypesOfCheese />
+    </>
+  );
+};
 class MyComponent extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  //   constructor(props) {
+  //     super(props);
+  //   }
   render() {
     return (
       <div>
         <h1>Hello World</h1>
         <h2>This is a class based compoent</h2>
+        {/* <ChildComponent /> */}
+        <Cheese />
       </div>
     );
   }
