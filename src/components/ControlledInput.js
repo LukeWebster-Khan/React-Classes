3bf0bc3859cc586878b2e2bee0dce73b3721ed8e
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-10-16 00:18:42 +0100

    created controlled form

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -5,12 +5,20 @@ class ControlledInput extends React.Component {
     super(props);
     this.state = {
       input: "",
+      submit: "",
     };
     this.controlFunc = this.controlFunc.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   controlFunc(event) {
     this.setState({
       input: event.target.value,
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      submit: this.state.input,
     });
   }
   render() {
@@ -19,6 +27,14 @@ class ControlledInput extends React.Component {
         <h1>Controlled Input</h1>
         <input onChange={this.controlFunc} />
         <h4>Controlled state:{this.state.input}</h4>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            This is a form. When the submit function runs it will update the
+            below
+          </p>
+          <button type="submit">Submit</button>
+          <h4>{this.state.submit}</h4>
+        </form>
       </>
     );
   }
